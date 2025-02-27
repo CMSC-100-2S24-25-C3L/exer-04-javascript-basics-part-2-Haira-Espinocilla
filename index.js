54bee6d44185755a95f7f8d222a40edbf4c27afe
User <hdespinocilla@up.edu.ph>
2025-02-27 14:57:33 +0800

    updated

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 /*import packages*/
-const fs = require('fs');
-const { v4: uuid4 } = require('uuid');
-const validator = require('validator');
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import validator from 'validator';
 
-function generateUniqueID(firstName, lastName) {
+function generateUniqueID(firstName, lastName) { 
     if (typeof firstName !== 'string' || typeof lastName !== 'string' || !firstName || !lastName) {
         throw new Error ("Invalid input!!!");
     }
 
     return firstName[0].toLowerCase() + lastName.toLowerCase() + uuidv4().replace(/-/g, '').substring(0, 8);
+    //example: aturing16271c5
     //reference: https://www.w3schools.com/jsref/jsref_replace.asp
     //reference: https://www.w3schools.com/jsref/jsref_substring.asp
 }
 
 function addAccount(userArray) {
     if(!Array.isArray(userArray) || userArray.length !== 4) {
-        return false;
+        return false; //if not within requirements
     }
 
-    const [firstName, lastName, email, age] = userArray;
+    const [firstName, lastName, email, age] = userArray; //array containing user info
 
     if (typeof firstName !== 'string' || !firstName.trim() || typeof lastName !== 'string' || !lastName.trim() || typeof email !== 'string' || !email.trim() || !validator.isEmail(email) || typeof age !== 'number' || age < 18 ) {
-        return false;
+        return false; //if not within requirements
     }
 
     const uniqueID = generateUniqueID (firstName, lastName);
-    const userData = '${firstName}, ${lastName}, ${email}, ${age}, ${uniqueID}\n';
+    const userData = `${firstName}, ${lastName}, ${email}, ${age}, ${uniqueID}\n`; //format of what to put in the users.txt
 
     fs.appendFileSync('users.txt', userData, 'utf8');
 
-    return true;
+    return true; //return true if successful
 }
 
-module.exports = { generateUniqueID, addAccount };
+export {generateUniqueID, addAccount};
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-const {generateUniqueID, addAccount} = require('./index');
+import { generateUniqueID, addAccount } from './index.js';
 
 console.log(generateUniqueID("Alan", "Turing"));
 
 console.log(addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]));
 console.log(addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]));
+console.log(addAccount(["Haira", "Espinocilla", "myEmail", 21]));
+console.log(addAccount(["Marie", "Dela Cruz", "haira.espinocilla@gmail.com", 15]));
