ec6d19b4bc60d7368af7f5fe659cdf210c9c4a8b
Haira-Espinocilla <hdespinocilla@up.edu.ph>
2025-02-27 15:18:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function generateUniqueID(firstName, lastName) {
 
 function addAccount(userArray) {
     if(!Array.isArray(userArray) || userArray.length !== 4) {
-        return false; //if not within requirements
+        return false; //if not within requirement
     }
 
     const [firstName, lastName, email, age] = userArray; //array containing user info
 
     if (typeof firstName !== 'string' || !firstName.trim() || typeof lastName !== 'string' || !lastName.trim() || typeof email !== 'string' || !email.trim() || !validator.isEmail(email) || typeof age !== 'number' || age < 18 ) {
-        return false; //if not within requirements
+        return false; //if not within requirement
     }
 
     const uniqueID = generateUniqueID (firstName, lastName);
